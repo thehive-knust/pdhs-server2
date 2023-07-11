dcbc4f5d9d0b6bf173b9fe0e1acb5c12cf2499aa
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2023-07-11 10:17:47 +0000

    Updated college_routes.py

--- src/pdhs_app/blueprints/college_routes.py
+++ src/pdhs_app/blueprints/college_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify, render_template
-from src.pdhs_app.models.users.user import User  
-from src.pdhs_app.models.colleges.college import College
+from pdhs_app.models.users.user import User  
+from pdhs_app.models.colleges.college import College
 
 bp = Blueprint('colleges', __name__, url_prefix='/colleges')
 
