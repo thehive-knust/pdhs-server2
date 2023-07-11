2d53fbd7ca21eb3045884c6c90315c245e098ea5
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2023-07-11 10:10:13 +0000

    Updated security.py

--- src/middleware/security.py
+++ src/middleware/security.py
@@ -1,5 +1,5 @@
 from werkzeug.security import safe_str_cmp
-from src.pdhs_app.models.users.user import User
+from pdhs_app.models.users.user import User
 from flask_jwt_extended import JWTManager
 
 jwt = JWTManager()
