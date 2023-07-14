804ba50e59425ee26dcf0fa03976f377b2a0b5ea
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2023-07-14 13:05:45 +0000

    Updated cloud_upload.py

--- src/middleware/cloud_upload.py
+++ src/middleware/cloud_upload.py
@@ -5,15 +5,10 @@ from cloudinary import uploader
 # from cloudinary.utils import cloudinary_url
 
 
-from dotenv import load_dotenv
-
-project_folder = os.path.expanduser('~/pdhs-server')  # adjust as appropriate
-load_dotenv(os.path.join(project_folder, '.env'))
-
 cloudinary.config(
-    cloud_name =  os.getenv('CLOUD_NAME'),   
-    api_key = os.getenv('API_KEY'),   
-    api_secret = os.getenv('API_SECRET') 
+    cloud_name = "thehivecloudstorage",    #os.getenv('CLOUD_NAME'),   
+    api_key = "134419777876826",    #os.getenv('API_KEY'),   
+    api_secret = "ZBzzFoC0br9z2jCKiHRxA_EkRQ0"    #os.getenv('API_SECRET') 
 )
 
 def upload_file(file_to_upload):
